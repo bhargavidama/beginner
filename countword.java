ff5d263c48180e82de460a6929d7a902fe87f45c
bhargavidama <41729354+bhargavidama@users.noreply.github.com>
2018-08-20 11:39:17 +0530

    Update countword.java

--- countword.java
+++ countword.java
@@ -14,25 +14,25 @@ class Ideone
 		n=sc.nextInt();
 		switch(n)
 		{
-			case 1:System.out.print("one");
+			case 1:System.out.print("One");
 			break;
-			case 2:System.out.print("two");
+			case 2:System.out.print("Two");
 			break;
-			case 3:System.out.print("three");
+			case 3:System.out.print("Three");
 			break;
-			case 4:System.out.print("four");
+			case 4:System.out.print("Four");
 			break;
-			case 5:System.out.print("five");
+			case 5:System.out.print("Five");
 			break;
-			case 6:System.out.print("six");
+			case 6:System.out.print("Six");
 			break;
-			case 7:System.out.print("seven");
+			case 7:System.out.print("Seven");
 			break;
-			case 8:System.out.print("eight");
+			case 8:System.out.print("Eight");
 			break;
-			case 9:System.out.print("nine");
+			case 9:System.out.print("Nine");
 			break;
-			case 10:System.out.print("ten");
+			case 10:System.out.print("Ten");
 			break;
 			
 		}
