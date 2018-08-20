0915dc63a8116eea52de9c7f5aadfe5c204b7202
bhargavidama <41729354+bhargavidama@users.noreply.github.com>
2018-08-20 11:33:13 +0530

    Create countword.java

---
+++ countword.java
@@ -0,0 +1,40 @@
+/* package whatever; // don't place package name! */
+
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+/* Name of the class has to be "Main" only if the class is public. */
+class Ideone
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+		int n;
+		Scanner sc=new Scanner(System.in);
+		n=sc.nextInt();
+		switch(n)
+		{
+			case 1:System.out.print("one");
+			break;
+			case 2:System.out.print("two");
+			break;
+			case 3:System.out.print("three");
+			break;
+			case 4:System.out.print("four");
+			break;
+			case 5:System.out.print("five");
+			break;
+			case 6:System.out.print("six");
+			break;
+			case 7:System.out.print("seven");
+			break;
+			case 8:System.out.print("eight");
+			break;
+			case 9:System.out.print("nine");
+			break;
+			case 10:System.out.print("ten");
+			break;
+			
+		}
+	}
+}
