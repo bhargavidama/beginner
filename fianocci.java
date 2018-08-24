3508b8b94f7a0d2aaa3a8bfe799a9bcb98ca8911
bhargavidama <41729354+bhargavidama@users.noreply.github.com>
2018-08-24 10:52:19 +0530

    Create fianocci.java

---
+++ fianocci.java
@@ -0,0 +1,26 @@
+/* package whatever; // don't place package name! */
+
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+/* Name of the class has to be "Main" only if the class is public. */
+class Ideone
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+		 int febCount;
+Scanner sc=new Scanner(System.in);
+febCount=sc.nextInt();
+         int[] feb = new int[febCount];
+         feb[0] = 1;
+         feb[1] = 1;
+         for(int i=2; i < febCount; i++){
+             feb[i] = feb[i-1] + feb[i-2];
+         }
+ 
+         for(int i=0; i< febCount; i++){
+                 System.out.print(feb[i] + " ");
+	}
+}
+}
