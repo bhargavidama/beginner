640de26fefcb03138f94ee5e08ec7fe23d44763c
bhargavidama <41729354+bhargavidama@users.noreply.github.com>
2018-09-18 09:57:10 +0530

    Create countnumeric.java

---
+++ countnumeric.java
@@ -0,0 +1,29 @@
+/* package whatever; // don't place package name! */
+
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+/* Name of the class has to be "Main" only if the class is public. */
+class Ideone
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+		String str;
+		int c=0;
+		char num;
+		Scanner sc=new Scanner(System.in);
+		str=sc.nextLine();
+		for(int i=0;i<str.length();i++){
+			
+			num=str.charAt(i);
+			if(Character.isDigit(num)){
+			c++;	
+			
+			
+			}
+			
+		}
+		System.out.print(c);
+	}
+}
